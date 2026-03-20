bc7e2e88996f591faf03c26acd85bb5cac8afda1
Jezerav <jeremy.robateau09@gmail.com>
2026-03-20 08:48:33 -0600

    Creacion de clases y distribucion de trabajo

---
+++ src/lab8_memoria/Nodoarchivo.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lab8_memoria;
+
+/**
+ *
+ * @author jerem
+ */
+public class Nodoarchivo {
+    
+}
